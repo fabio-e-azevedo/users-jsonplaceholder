cb57982a3d61e7b41767776cb4388554348546e5
Fabio Azevedo <emaildofabioeduardo@gmail.com>
2024-04-18 02:55:04 -0300

    onde tudo começou...

---
+++ go.mod
@@ -0,0 +1,3 @@
+module users-jsonplaceholder
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+type User struct {
+	Id       int32  `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Address  struct {
+		Street  string `json:"street"`
+		Suite   string `json:"suite"`
+		City    string `json:"city"`
+		ZipCode string `json:"zipcode"`
+		Geo     struct {
+			Lat string `json:"lat"`
+			Lng string `json:"lng"`
+		}
+	}
+	Phone   string `json:"phone"`
+	Website string `json:"website"`
+	Company struct {
+		Name        string `json:"name"`
+		CatchPhrase string `json:"catchPhrase"`
+		Bs          string `json:"bs"`
+	}
+}
